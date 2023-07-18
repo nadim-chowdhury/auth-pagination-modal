c73b4b3dd758dfe4fb5b75db82bff0aab43a7f99
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-07-18 23:28:50 +0600

    dnd added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function () {
   const [rData, setrData] = useState([]);
@@ -10,14 +11,44 @@ export default function () {
       .then((res) => setrData(res.data));
   }, []);
 
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const { source, destination } = result;
+
+    const newData = Array.from(rData);
+    const [removed] = newData.splice(source.index, 1);
+    newData.splice(destination.index, 0, removed);
+
+    setrData(newData);
+  };
+
   return (
     <>
-      {rData.map((d) => (
-        <div key={d.id} class="user">
-          <h3>{d.name}</h3>
-          <p>{d.email}</p>
-        </div>
-      ))}
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="droppable1">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {rData.map((d, i) => (
+                <Draggable key={d.id} draggableId={d.id.toString()} index={i}>
+                  {(provided) => (
+                    <div
+                      className="user"
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      {...provided.draggableProps}
+                    >
+                      <h3>{d.name}</h3>
+                      <p>{d.email}</p>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   );
 }
